086044b0cdcc9b37725a5ede564602eb3010431c
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2023-04-08 23:26:22 +0700

    init project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Cropper from 'svelte-easy-crop'
+
+  let image = '/images/dog.jpeg'
+  let crop = { x: 0, y: 0 }
+  let zoom = 1
+</script>
+
+<Cropper
+ {image}
+ bind:crop
+ bind:zoom
+ on:cropcomplete={e => console.log(e.detail)}
+/>
