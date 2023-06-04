e1be2774fb5670724d13fad5d6afa3b9d2418301
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2023-06-04 18:47:52 +0700

    fix footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <footer class="w-full p-4 flex flex-col items-center flex-wrap gap-3">
-	<a href="https://github.com/saenyakorn/profile-frame">
+	<a href="https://github.com/WebmasterCamp/profile-frame" class="flex items-center gap-2 flex-col">
 		<img alt="GitHub" src={logo} width="20" height="20" />
+		<p class="text-xs text-zinc-700">WebmasterCamp © 2022</p>
 	</a>
-	<p class="text-xs text-zinc-700">Saenyakorn Siangsanoh © 2022</p>
 </footer>
