fad23cfc31ab599dad5d0f75156c25c202b105b0
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2023-06-08 14:52:34 +0700

    change frames and add og image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,5 +3,13 @@
 	import Footer from '../components/Footer.svelte';
 </script>
 
+<svelte:head>
+	<title>YWC Frame</title>
+	<link rel="icon" type="image/png" href="/favicon.png" />
+	<meta name="description" content="YWC Frame based on your branch" />
+	<meta name="og:title" content="YWC Frame" />
+	<meta name="og:description" content="YWC Frame based on your branch" />
+	<meta name="og:image" content="/og.png" />
+</svelte:head>
 <slot />
 <Footer />
